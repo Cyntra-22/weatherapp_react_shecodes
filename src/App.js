914a8af888c827b3dc7bb19d742076e704e3e6db
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-12-05 15:18:42 +0700

    Updated the app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather
+      <footer>
+        This project was coded by{" "}
+        <a href="https://grand-moxie-36c236.netlify.app/">Myo Pa Pa Kyaw </a>
+        and it is
+        <a href="https://github.com/Cyntra-22/weatherapp_react_shecodes">
+          open-source-code.
+        </a>
+      </footer>
     </div>
   );
 }

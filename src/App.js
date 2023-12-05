e58ea1c976db4ede8bd1190c800e91a8543c0750
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-12-05 16:16:26 +0700

    Updated the code for json and content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
+import Weather from "./Weather"
 
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://grand-moxie-36c236.netlify.app/">Myo Pa Pa Kyaw </a>
-        and it is
-        <a href="https://github.com/Cyntra-22/weatherapp_react_shecodes">
-          open-source-code.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather/>
+        
+        <footer>
+          This project was coded by{" "}
+          <a href="https://grand-moxie-36c236.netlify.app/">Myo Pa Pa Kyaw </a>
+          and it is
+          <a href="https://github.com/Cyntra-22/weatherapp_react_shecodes">
+            open-source-code.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
